a1b6a11fde54bfa851c2f8c3ed539983bf5cc4fb
fabalcu97 <fballoncuadros@gmail.com>
2016-09-20 20:15:24 -0500

    Implementado método PUSH

--- Pila.h
+++ Pila.h
@@ -6,36 +6,33 @@ template<class T>
 struct Node{
 	T data;
 	Node * ptrNext;
-
 	Node( Node * nptrNext , T ndata)
 	{
-		data = ndata;
-		ptrNext  = nptrNext;
+	data = ndata;
+	ptrNext  = nptrNext;
 	}
-
 	Node(){}
 };
 
 template<class T>
 struct Stacki
 {
-	Node<T> * Head;
-	Node<T> * Tail;
+	Node<T>* Head;
+	Node<T>* Tail;
 
 	Stacki(){
 		Head = Tail = NULL;
 	}
 
 	bool Push(T data){
-
-		if( Head == NULL ){
+		if ( Head == NULL ){
 			Head = Tail = new Node<T>(NULL, data);
 			return true;
 		}
 		Tail->ptrNext = new Node<T>(NULL, data);
 		Tail = Tail->ptrNext;
 		return true;
-	}
+	};
 
 	bool Pop();
 	void Print();
